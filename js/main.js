6dd4bce0158f09fd218fa6df16af3e7536e2c50e
Jeroo <SALva18??>
2018-04-30 17:33:38 -0400

    Carga de imagenes desde una url local

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-$( document ).ready(function() {
-    $( document ).tooltip();
+$(document).ready(function () {
+  $(document).tooltip();
+
+  var container = $('.faimg'), inputFile = $('#file');	
+			
+	container.on('click', function(){	
+		inputFile.click();
+	});
+
+	inputFile.on('change', function(e){
+		
+    var filesSelected = this.files;
+    if (filesSelected.length > 0) {
+      var fileToLoad = filesSelected[0];
+  
+      if (fileToLoad.type.match("image.*")) {
+        var fileReader = new FileReader();
+        fileReader.onload = function (fileLoadedEvent) {
+          var imageLoaded = document.createElement("img");
+          imageLoaded.src = fileLoadedEvent.target.result;
+          document.getElementById("editor-textarea").appendChild(imageLoaded);
+        };
+        fileReader.readAsDataURL(fileToLoad);
+      }
+    }
+		
+ }); 
+
 });
 
 
@@ -11,5 +37,37 @@ function initDoc() {
 }
 
 function formatDoc(sCmd, sValue) {
-    document.execCommand(sCmd, false, sValue);
+  document.execCommand(sCmd, false, sValue);
+}
+
+
+function main() {
+  var inputFileToLoad = document.createElement("input");
+  inputFileToLoad.type = "file";
+  inputFileToLoad.id = "inputFileToLoad";
+  document.body.appendChild(inputFileToLoad);
+
+  var buttonLoadFile = document.createElement("button");
+  buttonLoadFile.onclick = loadImageFileAsURL;
+  buttonLoadFile.textContent = "Load Selected File";
+  document.body.appendChild(buttonLoadFile);
+}
+
+function loadImageFileAsURL() {
+  var filesSelected = document.getElementById("inputFileToLoad").files;
+  if (filesSelected.length > 0) {
+    var fileToLoad = filesSelected[0];
+
+    if (fileToLoad.type.match("image.*")) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (fileLoadedEvent) {
+        var imageLoaded = document.createElement("img");
+        imageLoaded.src = fileLoadedEvent.target.result;
+        document.getElementById("editor-textarea").appendChild(imageLoaded);
+      };
+      fileReader.readAsDataURL(fileToLoad);
+    }
   }
+}
+
+
