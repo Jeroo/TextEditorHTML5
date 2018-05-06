acaf635f09f7feb7e0f705fe3a5af48a71ad7f4e
Jeroo <SALva18??>
2018-05-05 20:57:40 -0400

    Se puso todo el codigo a vanilla JS

--- js/main.js
+++ js/main.js
@@ -1,40 +1,95 @@
+//Inicializamos Jquery para usar JQueryUI una vez cargue el documento completo
 $(document).ready(function () {
+ 
+  //Inicializamos la funcion tooltip de JQueryUI
   $(document).tooltip();
-  
-  var container = $('.faimg'), inputFile = $('#file');	
-			
-	container.on('click', function(){	
-		inputFile.click();
-	});
-
-	inputFile.on('change', function(e){
-		
-    var filesSelected = this.files;
-    if (filesSelected.length > 0) {
-      var fileToLoad = filesSelected[0];
-  
-      if (fileToLoad.type.match("image.*")) {
-        var fileReader = new FileReader();
-        fileReader.onload = function (fileLoadedEvent) {
-          var imageLoaded = document.createElement("img");
-          imageLoaded.src = fileLoadedEvent.target.result;
-          document.getElementById("editor-textarea").appendChild(imageLoaded);
-        };
-        fileReader.readAsDataURL(fileToLoad);
-      }
-    }
-		
- }); 
 
 });
 
-
 var oDoc, sDefTxt;
+//Inicializamos los eventos de javascript cuando carga todo el documento
+window.onload = function(){
 
 function initDoc() {
   oDoc = document.getElementById("editor-textarea");
   sDefTxt = oDoc.innerHTML;
+
+  return false;
 }
+
+initDoc();
+var container =  document.getElementById('faimgid'), idSaveDoc = document.getElementById('idSaveDoc'), idOpenDoc = document.getElementById('idOpenDoc'), inputFile = document.getElementById('file');	
+
+container.addEventListener('click', clickInputFile, false);
+
+function clickInputFile() {
+
+  inputFile.click();
+
+  return false;
+}
+
+
+inputFile.addEventListener("change",function (e) {
+  
+  var filesSelected = this.files;
+  if (filesSelected.length > 0) {
+    var fileToLoad = filesSelected[0];
+
+    if (fileToLoad.type.match("image.*")) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (fileLoadedEvent) {
+        var imageLoaded = document.createElement("img");
+        imageLoaded.src = fileLoadedEvent.target.result;
+        oDoc.appendChild(imageLoaded);
+      };
+      fileReader.readAsDataURL(fileToLoad);
+    }
+  }
+
+  return false;
+
+});
+
+idSaveDoc.addEventListener("click",function (e) { 
+
+  var documento = oDoc.innerHTML;
+  saveInLocalStored(documento);
+  return false;
+
+},false);
+
+
+idOpenDoc.addEventListener("click",function (e) { 
+
+      if (localStorage.getItem('documentoGuardado') != "" && localStorage.getItem('documentoGuardado') != undefined && localStorage.getItem('documentoGuardado') != null) {
+          
+        // Code for localStorage/sessionStorage.
+        oDoc.innerHTML = localStorage.getItem('documentoGuardado');
+        
+    
+      } else {
+    
+          alert("No tiene documentos guardados!");
+        
+      }  
+
+ },false);
+
+
+
+
+}
+
+function saveInLocalStored(data) {
+ 
+  localStorage.clear();
+  localStorage.documentoGuardado = data;
+  alert("Documento Guardado Con éxito!");
+
+  return false;  
+}
+
 
 function formatDoc(sCmd, sValue) {
   document.execCommand(sCmd, false, sValue);
@@ -51,55 +106,4 @@ function main() {
   buttonLoadFile.onclick = loadImageFileAsURL;
   buttonLoadFile.textContent = "Load Selected File";
   document.body.appendChild(buttonLoadFile);
-}
-
-function loadImageFileAsURL() {
-  var filesSelected = document.getElementById("inputFileToLoad").files;
-  if (filesSelected.length > 0) {
-    var fileToLoad = filesSelected[0];
-
-    if (fileToLoad.type.match("image.*")) {
-      var fileReader = new FileReader();
-      fileReader.onload = function (fileLoadedEvent) {
-        var imageLoaded = document.createElement("img");
-        imageLoaded.src = fileLoadedEvent.target.result;
-        imageLoaded.classList.add("resize");
-        document.getElementById("editor-textarea").appendChild(imageLoaded);
-      };
-      fileReader.readAsDataURL(fileToLoad);
-    }
-  }
-}
-
-function insertHTML(img) {
-  var id = "rand" + Math.random();
-  var doc = document.getElementById("editor");
-  doc = doc.document ? doc.document : doc.contentWindow.document;
-  img = "<img src='" + img + "' id=" + id + ">";
-
-  if(document.all) {
-    var range = doc.selection.createRange();
-    range.pasteHTML(img);
-    range.collapse(false);
-    range.select();
-  } else {
-    doc.execCommand("insertHTML", false, img);
-  }
-  return doc.getElementById(id);
-};
-
-function appendToStorage(name, data) {
-  
-  localStorage.clear();
-  localStorage.setItem(name,data);
-}
-
-function addTextArea() {
-
-  appendToStorage('documento',  $('#editor-textarea').html());
-}
-
-function openTextArea() {
-
-  $('#editor-textarea').html(localStorage.getItem('documento'));
 }
